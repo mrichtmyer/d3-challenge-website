e0d26e1ebdcc88601841b754dc3bfb79c4787653
Matthew Richtmyer <mrichtmyer94@gmail.com>
2020-04-18 19:58:58 -0400

    correct id for chart placement in index.html and app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
