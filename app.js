9d3e5501347b5fcd772ce4af03b098884b5c751b
Matthew Richtmyer <mrichtmyer94@gmail.com>
2020-04-18 19:55:55 -0400

    Correct data file path in d3 call

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var svg = d3.select(".chart")
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-d3.csv("/assets/data/data.csv").then(function(data,err){
+d3.csv("data.csv").then(function(data,err){
     if(err) throw err;
 
     //console.log(data)
